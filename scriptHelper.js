ec58da40ac7e6ff20cf347bd7d110d9dd725cf06
JAlycee <124473879+JAlycee@users.noreply.github.com>
2023-11-13 20:05:26 -0600

    updated 11/13/23

--- scriptHelper.js
+++ scriptHelper.js
@@ -33,16 +33,49 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
         validateInput(cargoLevel) ==="Empty") {
         alert("All fields are required!");
         return;
-}
+        }
+    }
+
 //Check if fuelLevel and cargoLevel are numbers
 if (validateInput(fuelLevel) !== "Is a Number" ||
     validateInput(cargoLevel) !== "Is a Number") {
     alert("Fuel level and cargo mass must be numbers!");
     return;
 
+// Update pilot and copilot status (template literal)
+document.getElementById("pilotStatus").innerText = 'Pilot ${pilot} is ready';
+document.getElementById("copilotStatus").innerText = 'copilot ${copilot} is ready';
 }
- }
- 
+
+// Check to see if fuel level is <10,000 kilograms
+if (Number(fuelLevel) <10000) {
+    isReadyForLaunch = false;
+    document.getElementById("fuelStatus").innerText = "There is not enough fuel for this journey";
+} else {
+    document.getElementById("fuelStatus").innerText ="Fuel level high enough for launch";
+}
+// Check cargo mass
+if (Number(cargoLevel)> 10000) {
+    isReadyForLaunch = false;
+    document.getElementById("cargoStatus").innerText = "Cargo mass too high for the shuttle to launch";
+} else {
+    document.getElementById("cargoStatus").innerText = "Cargo mass low enough for launch";
+}
+
+// Update launch status
+let launchStatus = document.getElementById("launchStatus");
+let faultyItems = document.getElementById("faultyItems");
+
+if (isReadyForLaunch) {
+    launchStatus.innerText = "Shuttle is ready for launch";
+    launchStatus.style.color = "green";
+    faultyItems.style.visibility = "hidden"
+} else {
+launchStatus.innerText = "Shuttle not ready for launch";
+launchStatus.style.color = "red";
+faultyItems.style.visibility = "visible";
+}
+
  async function myFetch() {
      let planetsReturned;
  
@@ -53,7 +86,12 @@ if (validateInput(fuelLevel) !== "Is a Number" ||
  }
  
  function pickPlanet(planets) {
- }
+    if (planets.length <5) {
+        return null;
+    } 
+    let randomIndex = Math.floor(Math.random() * planets.length);
+    return planets[randomIndex];
+    }
  
  module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
